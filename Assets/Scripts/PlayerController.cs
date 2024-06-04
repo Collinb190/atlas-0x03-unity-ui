b93e8e22c26c7491c0141d055b4e43d28aaeac27
Lin <100497279+Collinb190@users.noreply.github.com>
2024-06-04 10:43:08 -0500

    task 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float sprint;
+	private int score = 0;
 
 	private Rigidbody rb;
 
@@ -19,7 +20,18 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rb.velocity = movement * currentSpeed;
+        Vector3 movement = new Vector3(moveHorizontal * currentSpeed, rb.velocity.y, moveVertical * currentSpeed);
+		rb.velocity = movement;
+    }
+
+    // Handles score incrementation
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Pickup"))
+		{
+			score++;
+            Debug.Log($"Score: {score}");
+			Destroy(other.gameObject);
+        }
     }
 }
